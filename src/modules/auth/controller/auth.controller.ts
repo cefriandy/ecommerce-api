f9f9cc82bde94cdbf7d1930bcf82287584edda4a
Cefriandy Simarmata <cefriandysimarmata@gmail.com>
2025-05-17 12:12:05 +0700

    redirect

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,22 +1,34 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../service/auth.service';
 import { Public } from 'src/decorator/public.decorator';
+import { Request, Response } from 'express';
+
+interface AuthenticatedRequest extends Request {
+    user: any;
+}
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
+    @Get('profile')
+    @UseGuards(AuthGuard('jwt'))
+    getProfile(@Req() req: AuthenticatedRequest) {
+        return req.user;
+    }
+
     @Get('google')
     @Public()
     @UseGuards(AuthGuard('google'))
-    async googleAuth() {
-    }
+    async googleAuth() { }
 
     @Get('google/redirect')
     @Public()
     @UseGuards(AuthGuard('google'))
-    async googleAuthRedirect(@Req() req) {
-        return this.authService.handleGoogleRedirect(req.user);
+    async googleAuthRedirect(@Req() req: AuthenticatedRequest, @Res() res: Response) {
+        const result = await this.authService.handleGoogleRedirect(req.user);
+        res.cookie(result.cookie.name, result.cookie.value, result.cookie.options);
+        return res.redirect('http://localhost:5173/home');
     }
 }
